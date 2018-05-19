35a6eaebb15580788675e0444de0444c4aa7af22
Christopher.Wang <wangchao_ustb_2012@163.com>
2018-05-19 15:56:43 +0800

    解码器解决TCP粘包

--- src/main/java/io/github/christopher/netty/basic/TimeClient.java
+++ src/main/java/io/github/christopher/netty/basic/TimeClient.java
@@ -8,6 +8,8 @@ import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
 
 public class TimeClient {
 
@@ -20,7 +22,8 @@ public class TimeClient {
 
                         @Override
                         protected void initChannel(SocketChannel ch) throws Exception {
-                            ch.pipeline().addLast(new TimeClientHandler());
+                            ch.pipeline().addLast(new LineBasedFrameDecoder(1024)).addLast(new StringDecoder())
+                                    .addLast(new TimeClientHandler());
                         }
                     });
             ChannelFuture f = b.connect(host, port).sync();
